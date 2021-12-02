b3a1f11bee8e0c504a4a1288e47db4299a0bf744
Abeer Faizan <abeerfaizan@gmail.com>
2021-12-01 23:35:44 -0500

    removed node modules

--- src/client/components/ViewCars.jsx
+++ src/client/components/ViewCars.jsx
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-const ViewCars =  () => {
-const [cars, setCars] = 
- 
+const ViewCars = () => {
+    const [cars, setCars] = useState('');
+useEffect(() => {
+    fetch('/get-cars', {method:'GET'}).then(response => response.json()).then(data => setCars(data));
+});
     return (
-      
-<div>
-    <h1>View Cars Page</h1>
-<nav
-        style={{
-          borderBottom: "solid 1px",
-          marginLeft: "20px",
-          paddingBottom: "1rem"
-        }}
-      >
-        <Link to="/">View Cars</Link>
-      </nav>
-</div>
-    
+
+        <div>
+            <h1>View Cars Page</h1>
+            <nav
+                style={{
+                    borderBottom: "solid 1px",
+                    marginLeft: "20px",
+                    paddingBottom: "1rem"
+                }}
+            >
+                <Link to="/">Back to Home Page</Link>
+            </nav>
+            
+            <div>
+                {cars}
+            </div>
+        </div>
+
     );
-  
+
 }
 
 export default ViewCars;
